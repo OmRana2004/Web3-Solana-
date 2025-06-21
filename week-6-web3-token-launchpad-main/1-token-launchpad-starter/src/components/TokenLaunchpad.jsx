6a2e45b10b28fa5cc1df3eaf0806e2fd67a53d1e
OmRana2004 <omrana8755800322@gmail.com>
2025-06-21 23:46:01 +0530

    ./week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx

--- week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -21,4 +21,3 @@ export function TokenLaunchpad() {
 }
 //https://solana-labs.github.io/solana-web3.js/classes/Connection.html
 //https://solana-labs.github.io/solana-web3.js/classes/Keypair.html
-//https://solana-labs.github.io/solana-web3.js
