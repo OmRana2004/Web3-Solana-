c31fc845a18a4baa5089f81ebb4323ffdbd95b92
OmRana2004 <omrana8755800322@gmail.com>
2025-06-23 22:32:09 +0530

    ./  week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx

--- week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -21,8 +21,6 @@ export function TokenLaunchpad() {
 }
 // code 
 
-
-// The component is designed to be a part of a larger application that interacts with the Solana blockchain.
 // The `createToken` function is a placeholder for the logic that will handle the creation of the token on the Solana blockchain.
 // The component is a functional React component that returns a JSX structure.
 // The component is exported for use in other parts of the application.
