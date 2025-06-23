433ac1ba48ce939bb974deb2939d33b366563a72
OmRana2004 <omrana8755800322@gmail.com>
2025-06-23 22:31:01 +0530

    ./week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx

--- week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -21,7 +21,7 @@ export function TokenLaunchpad() {
 }
 // code 
 
-// It includes input fields for token name, symbol, image URL, and initial supply.
+
 // The `createToken` function is currently empty and should be implemented to handle the token creation logic.              
 // The component is styled to center the content vertically and horizontally within the viewport.
 // The `inputText` class is used for.
