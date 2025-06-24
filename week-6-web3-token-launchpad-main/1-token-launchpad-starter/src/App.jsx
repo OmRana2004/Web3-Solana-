86ac37e53ee9cbaccca3e4a1ddd57d73321f708f
OmRana2004 <omrana8755800322@gmail.com>
2025-06-24 23:50:04 +0530

    ./week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx | 2 +-

--- week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
+++ week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/App.jsx
@@ -42,7 +42,3 @@ export default App;
 //  https://100x-b-mcdn.akamai.net.in/cohort3/56/b684.ts
 
 //@ts-check   
-// hello
-//om i should be a good day
-// https://100x-b-mcdn.akamai.net.in/cohort3/56/b684.ts
-// https://100x-b-mcdn.
