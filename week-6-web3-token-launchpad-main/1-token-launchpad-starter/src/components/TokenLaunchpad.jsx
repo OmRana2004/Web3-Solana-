a097d0e314ba89f988dd5288a611ac14d886398f
OmRana2004 <omrana8755800322@gmail.com>
2025-07-05 12:25:36 +0530

    create a Token TokenLaunchpad

--- week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ week-6-web3-token-launchpad-main/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,36 +1,75 @@
-import { createMint } from "@solana/spl-token";
+import {
+  createInitializeMint2Instruction,
+  getMinimumBalanceForRentExemptMint,
+  MINT_SIZE,
+  TOKEN_PROGRAM_ID,
+} from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import {
+  Keypair,
+  SystemProgram,
+  Transaction,
+  PublicKey,
+} from "@solana/web3.js";
 
 export function TokenLaunchpad() {
+  const wallet = useWallet();
+  const { connection } = useConnection();
 
-    function createToken() {
-        const name = document.getElementById("name").value;
-        const symbol = document.getElementById("symbol").value;
-        const imageUrl = document.getElementById("imageUrl").value;
-        const initialSupply = document.getElementById("initalSupply").value;
+  async function createToken() {
+    const name = document.getElementById("name").value;
+    const symbol = document.getElementById("symbol").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+    const initialSupply = document.getElementById("initalSupply").value;
 
-        // console.log({
-        //     name,
-        //     symbol,
-        //     imageUrl,
-        //     initialSupply
-        // })
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    const keypair = Keypair.generate();
 
-        createMint();
-        
-    }
-    return  <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <h1>Solana Token Launchpad</h1>
-        <input id="name" className='inputText' type='text' placeholder='Name'></input> <br />
-        <input id="symbol" className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input id="imageUrl" className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input id="initalSupply" className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button onClick={createToken} className='btn'>Create a token</button>
+    const transaction = new Transaction().add(
+      SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: keypair.publicKey,
+        space: MINT_SIZE,
+        lamports,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMint2Instruction(
+        keypair.publicKey,
+        6, // decimals
+        wallet.publicKey,
+        null,
+        TOKEN_PROGRAM_ID
+      )
+    );
+
+    const { blockhash } = await connection.getLatestBlockhash();
+    transaction.recentBlockhash = blockhash;
+    transaction.feePayer = wallet.publicKey;
+
+    transaction.partialSign(keypair);
+    await wallet.sendTransaction(transaction, connection);
+
+    alert("Token creation transaction sent!");
+  }
+
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1>Solana Token Launchpad</h1>
+      <input id="name" className="inputText" type="text" placeholder="Name" /> <br />
+      <input id="symbol" className="inputText" type="text" placeholder="Symbol" /> <br />
+      <input id="imageUrl" className="inputText" type="text" placeholder="Image URL" /> <br />
+      <input id="initalSupply" className="inputText" type="text" placeholder="Initial Supply" /> <br />
+      <button onClick={createToken} className="btn">
+        Create a token
+      </button>
     </div>
+  );
 }
-
