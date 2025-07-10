e011b51b645d5ed9eaba26053aa60b26ad1e099a
OmRana2004 <omrana8755800322@gmail.com>
2025-07-10 12:16:16 +0530

    make a dapp using wagmi and react-query to connect to a wallet and send ETH

--- week11-ETH-Wallet-Adapters/src/App.jsx
+++ week11-ETH-Wallet-Adapters/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { http, createConfig, WagmiProvider, useConnect} from "wagmi"
+import { mainnet } from "wagmi/chains"
+import { injected } from "wagmi/connectors"
+
+const queryClient = new QueryClient()
+
+export const config = createConfig({
+  chains: [mainnet],
+  connectors: [
+    injected(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+  },
+})
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <WalletConnector />
+        <EthSend />
+      </QueryClientProvider>
+      </WagmiProvider>
   )
+}
+
+function WalletConnector() {
+  const { connectors, connect } = useConnect()
+  return connectors.map((connector) => (
+    <button key={connector.uid} onClick={() => connect({ connector })}>
+      {connector.name}
+    </button>
+  ))
+}
+
+  function EthSend() {
+
+    return <div>
+      <input type="text" placeholder="Address..."></input>
+      <button>Send 0.1 ETH</button>
+    </div>
 }
 
 export default App
